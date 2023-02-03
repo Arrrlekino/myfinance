c69a8b6a462e1f4926220b3aabe75d0ff53ff702
alla <zebrasteel@gmail.com>
2023-02-03 11:40:38 -0500

    refactoring

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -14,6 +14,7 @@ class OperationsController < ApplicationController
   # GET /operations/new
   def new
     @operation = Operation.new
+    @categories = Category.all()
   end
 
   # GET /operations/1/edit
