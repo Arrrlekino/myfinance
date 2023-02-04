0cd5d4246c9a13fa04b7a002e87dde021595c9c3
alla <zebrasteel@gmail.com>
2023-02-04 12:01:46 -0500

    add controller Main

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'operations#index'
+  get 'main/index'
+  root 'main#index'
   resources :operations
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
