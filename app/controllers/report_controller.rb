1dc69e5beaaa3dcd20d04eb3d77adc8b0180de8a
alla <zebrasteel@gmail.com>
2023-02-05 12:09:13 -0500

    change routes for reports

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,5 +6,12 @@ class ReportController < ApplicationController
   end
 
   def report_by_dates
+    operations_data = Operation.all.map { |op| [op.odate.to_s, op.exchange_operation] }
+    @dates = operations_odate.map { |e| e[0] }
+    @amounts = rates_data.map { |e| e[1] }
+
+    rates_data = Rate.all.map { |r| [r.rdate.to_s, r.exchange_rate] }
+    @dates = rates_data.map { |e| e[0] }
+    @rates = rates_data.map { |e| e[1] }  :amount, :odate,
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'report/index'
-  get 'report/report_by_category'
-  get 'report/report_by_dates'
+  
+  #get 'report/index'
+  get 'report/index', to: 'report/index'
+  #get 'report/report_by_category'
+  get 'report/report_by_category', to: 'report/index'
+  #get 'report/report_by_dates'
+  get 'report/report_by_dates', to: 'report/report_by_dates'
   get 'main/index'
   root 'main#index'
+  resources :report
   resources :operations
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
