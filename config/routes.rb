09b8c1e49dfa1f5bf2039c71f72a749a7c5c9f5f
alla <zebrasteel@gmail.com>
2023-02-05 12:54:40 -0500

    rename report  to reports

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  get 'reports/index'
+  get 'reports/report_by_category'
+  get 'reports/report_by_dates'
   
-  #get 'report/index'
-  get 'reports/index', to: 'reports/index'
-  #get 'report/report_by_category'
-  get 'reports/report_by_category', to: 'reports/index'
-  #get 'report/report_by_dates'
-  get 'reports/report_by_dates', to: 'reports/report_by_dates'
+
   get 'main/index'
   root 'main#index'
   #resources :reports
